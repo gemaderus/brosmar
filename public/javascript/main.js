8d85ebdb9f59222a64eb586328154b288eca2848
nobuti <buti@nobuti.com>
2015-02-18 10:42:40 +0100

    Syntax highlight.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,13 @@
 console.log("Congratulations! You are set!");
+
+var blocks = document.querySelectorAll("pre code");
+
+[].forEach.call(blocks, function(block){
+  var html = block.innerHTML;
+  html = html.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+  block.innerHTML = html;
+});
+
+console.log("escaped");
+
+Prism.highlightAll();
